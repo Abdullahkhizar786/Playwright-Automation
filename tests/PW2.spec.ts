b0fdf146aa23172a08994e2cfe94b867c6a5d71d
Abdullahkhizar786 <fa20-bsse-184@lgu.edu.pk>
2024-12-23 19:56:45 +0500

    complete task

--- tests/PW2.spec.ts
+++ tests/PW2.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Task no 1", () => {
-  test("Demo qa", async ({ page }) => {
+  test("TC01", async ({ page }) => {
     await page.goto("https://demoqa.com/");
     await expect(page).toHaveURL("https://demoqa.com/");
     await page.getByRole("heading", { name: "Elements" }).click();
@@ -36,4 +36,59 @@ test.describe("Task no 1", () => {
     await expect(page.locator("img").nth(3)).toBeVisible();
     await expect(page.locator("img").nth(3)).toBeVisible();
   });
+  test("TC03", async ({ page }) => {
+    await page.goto("https://demoqa.com/");
+    await page.getByRole("heading", { name: "Forms" }).click();
+    await page.getByText("Practice Form").click();
+    await page.getByPlaceholder("First Name").fill("Abdullah");
+    await page.getByPlaceholder("Last Name").fill("khizar");
+    await page.getByPlaceholder("name@example.com").fill("khizar@gmail.com");
+    await page.getByText("Male", { exact: true }).click();
+    await page.getByPlaceholder("Mobile Number").fill("0987654323");
+    await page.locator("#dateOfBirthInput").click();
+    await page.locator(".react-datepicker__month-select").selectOption("January");
+    await page.getByRole("combobox").nth(1).selectOption("2000");
+    await page.getByLabel("Choose Saturday, January 15th,").click();
+    const subjectsInput = page.locator('input[id="subjectsInput"]');
+    await subjectsInput.type("Maths", { delay: 100 });
+    await page.locator("#react-select-2-option-0").click();
+    await page
+      .locator("div")
+      .filter({ hasText: /^Music$/ })
+      .click();
+    await page.locator('input[type="file"]').setInputFiles("C:/Users/PC/Downloads/image (4).png");
+    await page.getByPlaceholder("Current Address").fill("lahore");
+    await page.locator("#state svg").click();
+    await page.getByText("Haryana").click();
+    await page.locator("#city svg").click();
+    await page.getByText("Panipat").click();
+    await page.getByRole("button", { name: "Submit" }).click();
+    await expect(page.getByText("Thanks for submitting the form")).toBeTruthy;
+  });
+
+  test("TC04", async ({ page }) => {
+    await page.goto("https://demoqa.com/");
+    await page.getByRole("heading", { name: "Widgets" }).click();
+    await page.locator("li").filter({ hasText: "Progress Bar" }).click();
+    await page.getByRole("button", { name: "Start" }).click();
+    await page.waitForTimeout(9000);
+    await page.getByText("%");
+    expect("%").toBeTruthy();
+  });
+  test("TC05", async ({ page }) => {
+    await page.goto("https://demoqa.com/");
+    await page.getByRole("heading", { name: "Widgets" }).click();
+    await page.locator("li").filter({ hasText: "Tool Tips" }).click();
+    await page.getByRole("button", { name: "Hover me to see" }).hover();
+    await page.getByText("You hovered over the Button");
+    expect("You hovered over the Button").toBeTruthy;
+  });
+  test("TC06", async ({ page }) => {
+    await page.goto("https://demoqa.com/");
+    await page.getByRole("heading", { name: "Interactions" }).click();
+    await page.locator("li").filter({ hasText: "Droppable" }).click();
+    await page.locator("#draggable").dragTo(page.getByLabel("Simple").locator("#droppable"));
+    expect(page.getByText("Dropped!")).toBeTruthy();
+  });
 });
+
