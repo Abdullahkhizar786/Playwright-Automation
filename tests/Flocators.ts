fdce3de7a2b9f9113bb959a340c8eeb1888e83b4
Abdullahkhizar786 <fa20-bsse-184@lgu.edu.pk>
2025-01-03 19:57:27 +0500

    updated

--- tests/Flocators.ts
+++ tests/Flocators.ts
@@ -1,10 +1,5 @@
 import { Page, expect } from "@playwright/test";
 export const Flocator = {
-  //   private page: Page;
-
-  //   constructor(page: Page) {
-  //     this.page = page;
-  //   }
   link: "https://www.flightnetwork.com/",
   Alink: "https://us-en.flightnetwork.com/?ibe.mass=1",
   Acceptall: 'role=button[name="Accept All"]',
@@ -14,10 +9,13 @@ export const Flocator = {
   lto: 'input[id="searchForm-singleBound-destination-input"]',
   Lhr: "#react-select-4-option-0",
   kara: "#react-select-7-option-1",
+  AlAss: '[class=" css-sdspv8-singleValue"]',
+  DAss: '[class="cm3pbi4 _1c374az1 cm3pbi3 _1c374az2"]',
   deptdate: '[id="singleBound.departureDate"]',
   selectpss: '[class="css-3q0md5"]',
   addadult: '[class="css-vqwtyu"]',
   addchild: '[class="css-vqwtyu"]',
   class: '[class="_1l9hnubc _1l9hnubb"]',
   searchflights: '[data-testid="searchForm-searchFlights-button"]',
+  
 };
--- tests/flightPoM.ts
+++ tests/flightPoM.ts
@@ -9,7 +9,7 @@ export class Flightpom {
 
   async toOpensite() {
     await this.page.goto(Flocator.link);
-    await expect(this.page.url()).toContain("flightnetwork")
+    await expect(this.page.url()).toContain("flightnetwork");
   }
 
   async acceptcookies() {
@@ -21,22 +21,27 @@ export class Flightpom {
     await this.page.locator(Flocator.togetsignup).click();
   }
   async tocheckflight(from: string, to: string, departdate: string, returndate: string) {
-    // await this.page.goto(Flocator.link);
-    // await expect(this.page).toHaveURL(Flocator.Alink);
+    await this.page.goto(Flocator.link);
     await this.page.waitForTimeout(3000);
     await this.page.locator(Flocator.lfrom).fill(from);
     await this.page.waitForTimeout(8000);
     await this.page.locator(Flocator.Lhr).click();
     await this.page.waitForTimeout(3000);
     await expect(this.page.locator(Flocator.lfrom)).toBeVisible();
+    await expect(this.page.locator(Flocator.AlAss).nth(0)).toContainText("Lahore");
     await this.page.locator(Flocator.lto).fill(to);
     await this.page.waitForTimeout(8000);
     await this.page.locator(Flocator.kara).click();
     await expect(this.page.locator(Flocator.lto)).toBeVisible();
+    await this.page.waitForTimeout(4000);
+    await expect(this.page.locator(Flocator.AlAss).nth(1)).toContainText("Al Kharj");
     await this.page.locator(Flocator.deptdate).click();
     await this.page.getByRole("gridcell", { name: departdate }).click();
+    await this.page.waitForTimeout(3000)
+    await expect(this.page.locator(Flocator.DAss).nth(0)).toHaveValue('2025-01-31')
     await this.page.getByLabel("Go to next month").click();
     await this.page.getByRole("gridcell", { name: returndate }).nth(0).click();
+    await expect(this.page.locator(Flocator.DAss).nth(1)).toHaveValue('2025-02-02')
     await this.page.locator(Flocator.selectpss).click();
     await this.page.locator(Flocator.addadult).nth(0).click();
     await this.page.locator(Flocator.addchild).nth(1).click();
@@ -44,11 +49,10 @@ export class Flightpom {
     await this.page.getByRole("option", { name: "First" }).click();
     await expect(this.page.getByText("Search flights")).toBeVisible();
     await this.page.locator(Flocator.searchflights).click();
-    await this.page.waitForTimeout(3000);
+    //await this.page.waitForTimeout(3000);
   }
   async rentalcars(loc: string, pickdate: string) {
-    // await this.page.goto(Flocator.link);
-    // await expect(this.page).toHaveURL(Flocator.Alink);
+    await this.page.goto(Flocator.link);
     await this.page.waitForTimeout(3000);
     await this.page.getByTestId("menu-link-Rental car").click();
     await this.page.locator('input[id="searchbox-toolbox-fts-pickup"]').fill(loc);
